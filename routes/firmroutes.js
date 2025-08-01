f2da9283efdd6d3308a56522c75b9084060c84c7
Devisri <yennidevisri13.7@gmail.com>
2025-08-01 08:33:39 +0530

    modified

--- routes/firmroutes.js
+++ routes/firmroutes.js
@@ -6,7 +6,7 @@ const router=express.Router();
 router.post('/add-firm',verifytoken,firmcontroller.addFirm);
 router.get('/uploads/:imageName',(req,res)=>{
     const imageName=req.params.imageName;
-res.headersSent('Content-Type', 'image/jpeg'); 
+res.setHeader('Content-Type', 'image/jpeg'); 
     res.sendFile(path.join(__dirname,'..','uploads',imageName))
 });
 
