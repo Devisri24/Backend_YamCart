fb046886ba177821679dba2f1a5521375a71c74e
Devisri <yennidevisri13.7@gmail.com>
2025-08-01 16:51:25 +0530

    Cors

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("MongoDB connected successfully!"))
 .catch((error)=>console.log((error)))
  // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true,
+}));
 app.use(bodyParser.json());
 app.use('/uploads', express.static('uploads'));
 
